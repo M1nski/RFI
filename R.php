0c58e6fe85bba6303995dd5688b9708ea19b8390
M1nski <129934727+M1nski@users.noreply.github.com>
2024-04-16 11:29:54 +0300

    Update R.php

--- R.php
+++ R.php
@@ -1,3 +1,12 @@
 <?php
-  echo "YO";
+// Chemin vers le répertoire courant
+$chemin_courant = './';
+
+// Utilise glob() pour obtenir la liste des fichiers et répertoires dans le répertoire courant
+$elements = glob($chemin_courant . '*', GLOB_MARK);
+
+// Affiche les éléments du répertoire courant
+foreach ($elements as $element) {
+    echo basename($element) . "<br>";
+}
 ?>
